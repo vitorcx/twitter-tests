3115ef8e807e434c0339cdacbf3bc1a32dbf6bbe
vitorcx <vitorcx010@gmail.com>
2018-11-25 19:34:25 -0200

    Starting tweet test

--- cypress/integration/twitter/twitter.spec.js
+++ cypress/integration/twitter/twitter.spec.js
@@ -9,3 +9,18 @@ describe('Twitter page', function(){
         cy.get('div.clearfix>button.submit').click()
     })
 })
+
+describe('User logged in features', function(){
+    beforeEach(function(){
+        cy.visit('https://twitter.com/') 
+        cy.get('.StaticLoggedOutHomePage-buttonLogin').click()
+        cy.get('div.clearfix.field>input.email-input').type('test33199455')
+        cy.get('div.clearfix>input.js-password-field').type('cypresstest')
+        cy.get('div.clearfix>button.submit').click()
+     })
+
+     it('create new tweet', function(){
+         cy.get('div#tweet-box-home-timeline').type('Test')
+         cy.get('div.TweetBoxToolbar-tweetButton>button[innerText="Tweet"]').click()
+     })
+})
