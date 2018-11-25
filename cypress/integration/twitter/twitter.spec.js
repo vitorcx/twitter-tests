99764a6779228784c75b7e7c3900de28195ea1ae
vitorcx <vitorcx010@gmail.com>
2018-11-25 18:59:59 -0200

    Adding login and access tests

--- cypress/integration/twitter/twitter.spec.js
+++ cypress/integration/twitter/twitter.spec.js
@@ -1,13 +1,11 @@
 describe('Twitter page', function(){
-    beforeEach(function(){
+    it('access aplication', function(){
         cy.visit('https://twitter.com/') 
+    })
+    it('Login test user', function(){
         cy.get('.StaticLoggedOutHomePage-buttonLogin').click()
         cy.get('div.clearfix.field>input.email-input').type('test33199455')
         cy.get('div.clearfix>input.js-password-field').type('cypresstest')
         cy.get('div.clearfix>button.submit').click()
-
-    })
-    it('follows Cypress_io user', function(){
-        
     })
 })
